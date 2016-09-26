fcd4cda2495892ebf65132d2101010f3ce293cf6
erwidman <erwidman@mtu.edu>
2016-09-25 22:56:02 -0400

    Testing

--- GolfDB/app/src/main/java/com/teamgolf/golfdb/DatabaseHelper.java
+++ GolfDB/app/src/main/java/com/teamgolf/golfdb/DatabaseHelper.java
@@ -1,33 +1,77 @@
 package com.teamgolf.golfdb;
 
 import android.content.Context;
+import android.content.res.AssetManager;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteException;
 import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
 
 /**
  * Created by Eric on 9/23/2016.
  */
 public class DatabaseHelper extends SQLiteOpenHelper {
 
-    public static final String dbPath ="/data/data/com.teamgolf.golfdb/assets/databases/";
+    public static final String dbPath ="data/data/com.teamgolf.golfdb/";
     public static final String dbName ="golf.db";
     public static final int dbVersion = 1;
     private Context context = null;
     private SQLiteDatabase db;
+    private String path = dbPath+ dbName;
 
     public DatabaseHelper(Context context){
         super(context,dbName,null,dbVersion);
         this.context = context;
     }
 
-    public SQLiteDatabase getDB(){
+    public SQLiteDatabase connectDB(){
+        //create file
+        if(! new File(path).exists()){
+            try{
+                FileOutputStream out = new FileOutputStream(path);
+                InputStream in =context.getAssets().open(dbName);
+                byte [] buffer = new byte[1024];
+                int readBytes = 0;
+
+                while((readBytes = in.read(buffer))!= -1){
+                    out.write(buffer,0,readBytes);
+                }
+                in.close();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+
+        //clos db if already connected
+        if(db != null && db.isOpen())
+            db.close();
+
+
+
         try{
-            String path = dbPath + dbName;
             this.db = SQLiteDatabase.openDatabase(path,null,SQLiteDatabase.OPEN_READWRITE);
         }catch(SQLiteException e){
             throw new Error("Improper Path");
         }
+
+        Cursor c = db.rawQuery("select * from courses",null);
+        Log.d("TEST","Running Query");
+        int tmp = c.getColumnCount();
+        Log.d("TEST",Integer.toString(tmp));
+
+
+
+
+
+
+
         return db;
     }
 
--- GolfDB/app/src/main/java/com/teamgolf/golfdb/Login.java
+++ GolfDB/app/src/main/java/com/teamgolf/golfdb/Login.java
@@ -1,10 +1,14 @@
 package com.teamgolf.golfdb;
 
 import android.content.Context;
+import android.content.res.AssetManager;
 import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+
+import java.io.IOException;
+import java.io.InputStream;
 
 public class Login extends AppCompatActivity {
 
@@ -14,15 +18,29 @@ public class Login extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+
+
+
+        //create login view
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+
         //initializations
         context = this.getApplicationContext();
         DatabaseHelper dbHelper = new DatabaseHelper(context);
-        db = dbHelper.getDB();
-        Log.e("DB","DB Loaded!");
-
-
+        dbHelper.connectDB();
+        Log.d("TEST_RUN","DB Loaded!");
 
     }
+
+
+
+    public boolean checkPassword(String user, String Password, SQLiteDatabase db){
+
+        return false;
+    }
+
+
 }
