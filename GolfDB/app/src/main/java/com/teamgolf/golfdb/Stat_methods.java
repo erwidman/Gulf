e6acef29ba939d348103f457aa60bc7f8725f3a2
Robby Reid <rareid@c28-0117-14.ad.mtu.edu>
2016-11-30 14:05:49 -0500

    Stat Methods

--- GolfDB/app/src/main/java/com/teamgolf/golfdb/Stat_methods.java
+++ GolfDB/app/src/main/java/com/teamgolf/golfdb/Stat_methods.java
@@ -5,81 +5,81 @@ package com.teamgolf.golfdb;
  */
 
 public class Stat_methods {
-    private int roundInformation[][];
-    private int courseInformation[];
-    private int roundTotal[];
-    int CoursePar = 0;
+	private int roundInformation[][];
+	
+	// rows are par, mens, womens, children in order
+	private int courseInformation[][];
+	private int roundTotal[];
+	private int CoursePar = 0;
 
-    public int AverageRoundScore()
-    {
-        int average = 0;
-        int totalHoles =  roundInformation.length * roundInformation[0].length;
-        roundTotal = new int[roundInformation.length];
-        for (int row= 0; row < roundInformation[0].length; row++)
-        {
-            for (int column = 0; column < roundInformation.length; column++) {
-                average += roundInformation[row][column];
-                roundTotal[row]+=roundInformation[row][column];
-            }
-        }
-        return average / totalHoles;
-    }
-    public int TimesPlayed (){
-        return roundTotal.length;
-    }
-    public int[] Percentages()
-    {
-        int[] percentages = new int[3];
+	public double AverageRoundScore() {
+		double average = 0;
+		int totalHoles = roundInformation.length * roundInformation[0].length;
+		roundTotal = new int[roundInformation.length];
+		for (int row = 0; row < roundInformation.length; row++) {
+			for (int column = 0; column < roundInformation[0].length; column++) {
+				average += roundInformation[row][column] + 0.0;
 
-        for (int row= 0; row < roundInformation[0].length; row++)
-        {
-            for (int column = 0; column < roundInformation.length; column++) {
-                int par = courseInformation[column];
-                CoursePar+=par;
-                int strokes = roundInformation[row][column];
+				roundTotal[row] += roundInformation[row][column];
+			}
+		}
+		return average / totalHoles;
+	}
 
-                if(par-strokes == 1){
-                    percentages[0]+=1;
-                }else if(par-strokes == 0){
-                    percentages[1] +=1;
-                }else if(par-strokes ==-1){
-                    percentages[2]+=1;
-                }
-            }
-        }
+	public int TimesPlayed() {
+		return roundTotal.length;
+	}
 
-        for (int i =0; i <3; i++){
-            percentages[i] = percentages[i] / (roundInformation.length * roundInformation[0].length);
-        }
-        return percentages;
-    }
+	public double[] Percentages() {
+		double[] percentages = new double[3];
+		roundTotal = new int[roundInformation.length];
+		for (int row = 0; row < roundInformation.length; row++) {
+			for (int column = 0; column < roundInformation[0].length; column++) {
 
-<<<<<<< HEAD
-    public double Handicap(){
-    	int averageOverPar = 0;
-    	if(CoursePar!=0){
-    		for(int i = 0; i < roundTotal.length; i++){
-    			averageOverPar+=roundTotal[i] - CoursePar;
-    		}
-    	}
-    	averageOverPar = averageOverPar / roundTotal.length;
-    	return averageOverPar*.8;
-    }
-=======
-//    public int Handicap(){
-//    	int averageOverPar = 0;
-//    	if(CoursePar!=0){
-//    		for(int i = 0; i < roundTotal.length; i++){
-//    			averageOverPar+=roundTotal[i] - coursePar;
-//    		}
-//    	}
-//    	averageOverPar = averageOverPar / roundTotal.length;
-//    	return averageOverPar*.8;
-//    }
->>>>>>> a1966f8a60ce1a24df01602950a7331dbf14eb23
+				int par = courseInformation[0][column];
+				int strokes = roundInformation[row][column];
+				roundTotal[row] += strokes;
 
-    public void Stat_methods(int[][] roundInformation,int[]  courseInformation){
-        this.roundInformation = roundInformation;
-        this.courseInformation = courseInformation;
-    }
+				if (par - strokes == 1) {
+					percentages[0] += 1.0;
+				} else if (par - strokes == 0) {
+					percentages[1] += 1.0;
+				} else if (par - strokes == -1) {
+					percentages[2] += 1.0;
+				}
+			}
+		}
+
+		for (int i = 0; i < 3; i++) {
+			percentages[i] = percentages[i]
+					/ (roundInformation.length * roundInformation[0].length);
+		}
+		return percentages;
+	}
+
+	public double Handicap() {
+		int averageOverPar = 0;
+		if (CoursePar != 0) {
+			for (int i = 0; i < roundTotal.length; i++) {
+				averageOverPar += roundTotal[i] - CoursePar;
+			}
+		}
+		averageOverPar = averageOverPar / roundTotal.length;
+		return averageOverPar * .8;
+	}
+	
+	public int Par () {
+		CoursePar=0;
+		for(int column = 0; column < courseInformation[0].length; column++){
+			CoursePar+=courseInformation[0][column];
+		}
+		
+		return CoursePar;
+	}
+
+	public Stat_methods(int[][] roundInformation, int[][] courseInformation) {
+		this.roundInformation = roundInformation;
+		this.courseInformation = courseInformation;
+	}
+
 }
